e6b01b1c8909632a9dcd7ac535ba85127f2c5cf9
Konstantin BIFERT <konstantin.bifert@gmail.com>
2021-12-22 06:31:07 +0100

    init

---
+++ src/pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useFetch } from '@vueuse/core'
+const users = ref([])
+const { data } = await useFetch(
+  'https://jsonplaceholder.typicode.com/users',
+)
+  .get()
+  .json()
+users.value = data.value
+</script>
+
+<template>
+  <div>
+    {{ users }}
+  </div>
+</template>
